693424879e9c01eb6b10cb7f9ea58b5889ceeb86
jsiewierski11 <jarid.siewierski@gmail.com>
2020-12-04 13:04:50 -0700

    Adding missing responses to the dataset. Hopefully now the encoder should be able to process the dataset.'

--- src/chatbot/main.py
+++ src/chatbot/main.py
@@ -15,6 +15,10 @@ if __name__ == "__main__":
     answers = []
     try:
         for row in datareader:
+            if not isinstance(row[0], str) or not isinstance(row[1], str):
+                print("row[0]: {}, {}".format(type(row[0]), row[0]))
+                print("row[1]: {}, {}".format(type(row[1]), row[1]))
+                exit()
             questions.append(row[0]) #= np.append(questions, row[0])
             answers.append(row[1]) #= np.append(answers, row[1])
     except Exception as e:
@@ -32,11 +36,11 @@ if __name__ == "__main__":
     chatbot.init_embeddings(questions, answers)
     # chatbot.pickle_embeddings(questions, answers)
 
-    queries = ["What is your name?", "How are you today Ryan?", "You just said that.", "Can you tell me a joke?", "I love a good adventure book.", "What year is it?"]
-    print("Asking questions...")
-    for query in queries:
-        query_embedding = chatbot.textembedder.create_sentence_embeddings(query)
-        response_embeddings, response_indexes = chatbot.answer_query(query_embedding)
-        print("##########")
-        print("User: {}".format(query))
-        print("Ryan: {}".format(answers[response_indexes[0]]))
+    # queries = ["What is your name?", "How are you today Ryan?", "You just said that.", "Can you tell me a joke?", "I love a good adventure book.", "What year is it?"]
+    # print("Asking questions...")
+    # for query in queries:
+    #     query_embedding = chatbot.textembedder.create_sentence_embeddings(query)
+    #     response_embeddings, response_indexes = chatbot.answer_query(query_embedding)
+    #     print("##########")
+    #     print("User: {}".format(query))
+    #     print("Ryan: {}".format(answers[response_indexes[0]]))
