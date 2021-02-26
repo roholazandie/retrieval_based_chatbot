3194f6e32249b4ac2f068b3dfc15b481a93b8911
jsiewierski11 <jarid.siewierski@gmail.com>
2021-02-26 14:59:22 -0700

    Putting tokenizer on cuda also.

--- src/chatbot/textembedder/textembedder.py
+++ src/chatbot/textembedder/textembedder.py
@@ -9,10 +9,11 @@ class TextEmbedder:
     def __init__(self, tokenizer_filepath, model_filepath):
         self._tokenizer = AutoTokenizer.from_pretrained(tokenizer_filepath)
         self._model = AutoModel.from_pretrained(model_filepath)
-        print("Created model: {}".format(type(self._model)))
         if torch.cuda.is_available():
-            print("Cuda is available, putting the pretrained model on the GPU.")
+            print("Cuda is available, putting the pretrained model and tokenizer on the GPU.")
+            self._tokenizer.to('cuda')
             self._model.to('cuda')
+
 
     @property
     def tokenizer(self):
@@ -26,7 +27,8 @@ class TextEmbedder:
         try:
             #Tokenize questions
             encoded_input = self.__create_encoding(document)
-            # print("Encoded input - {}".format(type(encoded_input)))
+            print("Encoded input - {}".format(type(encoded_input)))
+
             model_output = self.__compute_token_embedding(encoded_input)
             # print("Model output - {}".format(type(model_output)))
             # print("Model output 1- {}".format(type(model_output[0])))
@@ -58,7 +60,7 @@ class TextEmbedder:
         with torch.no_grad():
             return self._model(**encoded_input)
 
-    #Mean Pooling - Take attention mask into account for correct averaging
+    # Mean Pooling - Take attention mask into account for correct averaging
     def __mean_pooling(self, model_output, attention_mask):
         token_embeddings = model_output[0] #First element of model_output contains all token embeddings
         input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
