7c96508116d6762f33fb5fc938860212b5f64bdc
jsiewierski11 <jarid.siewierski@gmail.com>
2021-03-03 11:31:32 -0700

    adding new script.

---
+++ src/chatbot/cuda_example.py
@@ -0,0 +1,4 @@
+from transformers import AutoModel, AutoTokenizer
+tokenizer = AutoTokenizer.from_pretrained("sentence-transformers/bert-base-nli-mean-tokens")
+model = AutoModel.from_pretrained("sentence-transformers/bert-base-nli-mean-tokens")
+model.to("cuda:0")
