c5d6d7456f884bed7bf1ef8ef3d312e742d3c49b
jsiewierski11 <jarid.siewierski@gmail.com>
2021-03-11 15:01:11 -0700

    Changing batch size.

--- src/chatbot/textembedder/textembedder.py
+++ src/chatbot/textembedder/textembedder.py
@@ -38,7 +38,7 @@ class TextEmbedder:
             dataloader = DataLoader(TensorDataset(encoded_input['input_ids'],
                                                   encoded_input['token_type_ids'],
                                                   encoded_input['attention_mask']),
-                                    batch_size=400,
+                                    batch_size=300,
                                     shuffle=False, num_workers=0)
             print("Created dataloader")
 
